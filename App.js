14612bc41867b0b4f85034210f51d2e793d99b76
skaldat1 <skaldat1@binghamton.edu>
2019-02-07 01:05:50 -0500

    upper part done

--- App.js
+++ App.js
@@ -8,43 +8,64 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TouchableHighlight
+} from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+import AppStyles from './styles/style-app';
+
+const styles = StyleSheet.create(AppStyles);
+
 
 type Props = {};
 export default class App extends Component<Props> {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      gridSize: 3,
+      score: 0
+    };
+
+    this.handleResetButtonClicked = this.handleResetButtonClicked.bind(this);
+  }
+
+  handleResetButtonClicked() {
+    console.log("fjhfkdfgdfg");
+    let state = this.state;
+  }
+
+  getHeaderView() {
+    return (
+      <View style={styles.headerPanelContainer}>
+        <Image source={require('./styles/images/2048-animated-edition.gif')} style={styles.headerLabelImage}/>
+        <View style={styles.buttonsWrapper}>
+          <TouchableHighlight style={styles.resetButton} onPress={this.handleResetButtonClicked}>
+            <Text style={styles.resetText}>Reset</Text>
+          </TouchableHighlight>
+          <View style={styles.scoreBoardContainer}>
+            <Text style={styles.scoreLabel}>Score</Text>
+            <Text style={styles.scoreDynamic}>{this.state.score}</Text>
+          </View>
+        </View>
+      </View>
+    );
+  }
+
   render() {
+
+
+    let oHeaderView = this.getHeaderView();
+
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        {oHeaderView}
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
---
+++ styles/style-app.js
@@ -0,0 +1,68 @@
+export default {
+  container: {
+    height: '100%',
+    width: '100%',
+    backgroundColor: '#57407C'
+  },
+
+  headerPanelContainer: {
+    height: "50%",
+    width: "100%",
+  },
+
+  headerLabelImage: {
+    marginTop: 100,
+    height: "20%",
+    width: "100%"
+  },
+
+  buttonsWrapper:{
+    flex: 1,
+    flexDirection:"row",
+    width: "100%",
+    marginTop: 10
+  },
+
+  resetButton: {
+    width: 50,
+    height: 50,
+    backgroundColor: '#3d2963',
+    borderRadius: 5,
+    marginTop: 50,
+    marginLeft: 20,
+    marginRight: 60,
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  resetText:{
+    color: '#ffffff80',
+    fontSize: 15,
+    fontWeight: "bold"
+  },
+
+  scoreBoardContainer: {
+    width: 50,
+    height: 50,
+    backgroundColor: '#3d2963',
+    borderRadius: 5,
+    marginTop: 50,
+    marginRight: 20,
+    marginLeft: 20,
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+
+  scoreLabel: {
+    color: '#ffffff9f',
+    fontSize: 12,
+  },
+  scoreDynamic: {
+    color: '#fffffff8',
+    fontSize: 18,
+    fontWeight: "bold"
+
+  }
+}
